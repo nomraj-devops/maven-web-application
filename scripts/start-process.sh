91a04903082ec439b6fc9a014500cf0e2772aa8d
nomrajdevops <157715741+Nomrajdevops@users.noreply.github.com>
2024-10-06 20:06:01 +0530

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -3,10 +3,10 @@ sudo -u root aws ecr get-login-password --region us-east-1 | docker login --user
 sudo -u root docker rm -f `docker ps -q -a`
 sudo -u root docker rmi -f `docker images -q`
 sleep 3
-sudo -u root docker pull 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:latest
+sudo -u root docker pull 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:19
 if [[ $? != 0 ]]
 then
     sudo -u root aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 194973748467.dkr.ecr.us-east-1.amazonaws.com
-    sudo -u root docker pull 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:latest
+    sudo -u root docker pull 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:19
 fi
-sudo -u root docker run -d -p 8080:8080 --name mavenwebapp 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:latest
+sudo -u root docker run -d -p 8080:8080 --name mavenwebapp 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:19
