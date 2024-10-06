5d94dd1be8976eeaed5d758d8f21da438dd3ef52
nomrajdevops <157715741+Nomrajdevops@users.noreply.github.com>
2024-10-06 18:14:46 +0530

    Create start-process.sh

---
+++ scripts/start-process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo -u root aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 194973748467.dkr.ecr.us-east-1.amazonaws.com
+sudo -u root docker rm -f `docker ps -q -a`
+sudo -u root docker rmi -f `docker images -q`
+sleep 3
+sudo -u root docker pull 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:latest
+if [[ $? != 0 ]]
+then
+    sudo -u root aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 194973748467.dkr.ecr.us-east-1.amazonaws.com
+    sudo -u root docker pull 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:latest
+fi
+sudo -u root docker run -d -p 8080:8080 --name mavenwebapp 194973748467.dkr.ecr.us-east-1.amazonaws.com/samplen:latest
